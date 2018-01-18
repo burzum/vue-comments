78c3024e216af97a25e1ba3bd0498e60e7398083
Falk <fr@world-architects.com>
2018-01-18 14:57:38 +0100

    try to get tests with jest running

---
+++ test/unit/components/comment.spec.js
@@ -0,0 +1,42 @@
+import { mount, shallow } from 'vue-test-utils'
+import Comment from '@/components/comment.vue';
+
+const createCmp = propsData => mount(Comment, { propsData })
+
+describe('Comment.vue', () => {
+	let cmp;
+
+	beforeEach(() => {
+		console.log('foreach test called');
+
+		// cmp = createCmp({
+		// 	data: {
+		// 		comment: {
+		// 			id: 1,
+		// 			user: {
+		// 				id: 1,
+		// 				username: 'falk'
+		// 			},
+		// 			name: 'test name',
+		// 			body: 'some body text',
+		// 			created: '01-01-2018'
+		// 		},
+		// 		modelId: 1,
+		// 		level: 1,
+		// 		parentId: null,
+		// 		model: 'SomeModel'
+		// 	}
+		// })
+	});
+
+	it('has a message property', () => {
+		// expect(cmp.vm('modelId', '1')).toBeTruthy()
+		expect(true).toEqual(true);
+	})
+//
+// 	test('test 1', () => {
+//
+// 		console.log(createCmp);
+// 		expect(true).toEqual(true);
+// 	});
+});
--- test/unit/setup.js
+++
@@ -1,5 +0,0 @@
-import 'babel-polyfill'
-import Vue from '../../node_modules/vue/dist/vue.common'
-import Vuex from '../../node_modules/vuex/dist/vuex.common'
-
-Vue.use(Vuex);
